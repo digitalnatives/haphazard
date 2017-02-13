24662e6e0666fb2c5a65fc162b94d73f12ac2e39
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-02-13 17:32:20 -0200

    Add hex.pm metadata.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Haphazard.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -19,6 +21,24 @@ defmodule Haphazard.Mixfile do
     [
       {:plug, "~> 1.3"},
       {:credo, "~> 0.6.1", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
+
+  defp description do
+    """
+    A configurable plug for caching.
+    """
+  end
+
+  defp package do
+    [
+     name: :haphazard,
+     files: ["config", "lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Rodrigo Nonose"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/digitalnatives/haphazard"}
+    ]
+  end
+
 end
