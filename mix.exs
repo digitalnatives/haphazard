7cb6cf6ea4a9f5c2c17e09830aa0815781f3204a
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-03-08 13:06:42 -0300

    Setup Travis and Converall.io.

--- mix.exs
+++ mix.exs
@@ -9,7 +9,10 @@ defmodule Haphazard.Mixfile do
      start_permanent: Mix.env == :prod,
      description: description(),
      package: package(),
-     deps: deps()]
+     deps: deps(),
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+   ]
   end
 
   def application do
@@ -22,6 +25,7 @@ defmodule Haphazard.Mixfile do
       {:plug, "~> 1.3"},
       {:credo, "~> 0.6.1", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev},
+      {:excoveralls, "~> 0.6", only: :test},
     ]
   end
 
